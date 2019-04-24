fe910f63f895ea7c82166943c4c7ee52bbd5e979
Jaclyn <jaclyndshahan@gmail.com>
2019-04-24 14:50:34 -0500

    starting forum page

--- src/components/Forum.js
+++ src/components/Forum.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Icon } from 'antd';
 
 class Forum extends Component {
   constructor() {
@@ -6,7 +7,25 @@ class Forum extends Component {
     this.state = {};
   }
   render() {
-    return <div />;
+    return (
+        <div>
+        <div className='comment_block'>
+        </div>
+       <div className='create_new_comment'>
+       </div>
+       <div className='user_avatar'>
+       <img src="https://lh3.googleusercontent.com/-BOAg0QFuH-M/WIreaUkGHeI/AAAAAAAAABA/WeG7oJQh5oQAvEFxfnmQzjv5WGrWGOJ2ACEwYBhgL/w140-h140-p/9ff3cf93-6b2b-4d4f-a4a7-9af756f3a3da"/>
+       <div className="input_comment">
+       <input 
+        type="text"
+        placeholder="Join the Conversation"
+        />
+        </div>
+       </div>
+       </div>
+
+      
+    );
   }
 }
 
